0a18a0561489e713bad369e56d68fd5db506ed15
lerhxx <407785526@qq.com>
2016-11-09 12:31:15 +0800

    db

--- config/default.js
+++ config/default.js
@@ -5,5 +5,6 @@ module.exports = {
 		key: 'myblog',
 		maxAge: 2592000000
 	},
-	mongodb: 'mongodb://localhost:27017/blog'
+	// mongodb: 'mongodb://localhost:27017/blog'
+	mongodb: 'mongodb://x_x:123456@ds149207.mlab.com:49207/myblog'
 };
