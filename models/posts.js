da729ea5bbec3c47ad179563ea6fdd8c39981d64
lerhxx <407785526@qq.com>
2016-11-11 22:42:03 +0800

    es6 class

--- models/posts.js
+++ models/posts.js
@@ -3,162 +3,173 @@
 const mongodb = require('./db'),
 	markdown = require('markdown').markdown;
 
-function Post(name, title, abstract, content) {
-	this.name = name;
-	this.title = title;
-	this.abstract = abstract;
-	this.content = content;
-}
 
-module.exports = Post;
-
-Post.prototype.save = (callback) => {
-	let date = new Date();
-
-	let time = {
-		date: date,
-		year: date.getFullYear(),
-		month: date.getFullYear() + '-' + (date.getMonth() + 1),
-		day: date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate(),
-		minute: date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' ' + date.getHours() + ':' + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes())
+class Post {
+	constructor(name, title, abstract, content) {
+		this.name = name;
+		this.title = title;
+		this.abstract = abstract;
+		this.content = content;
 	}
 
-	let post = {
-		name: this.name,
-		time: time,
-		title: this.title,
-		abstract: this.abstract,
-		content: this.content,
-		like: 0,
-		comments: []
-	}
+	save(callback) {
+		let date = new Date();
 
-	mongodb.MongoClient.connect(mongodb.url, (err, db) => {
-		if(err) {
-			return callback(err);
+		let time = {
+			date: date,
+			year: date.getFullYear(),
+			month: date.getFullYear() + '-' + (date.getMonth() + 1),
+			day: date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate(),
+			minute: date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' ' + date.getHours() + ':' + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes())
 		}
-		let collection = db.collection('posts');
-		collection.insert(post, {safe: true}, err => {
-			db.close();
+
+		let post = {
+			name: this.name,
+			time: time,
+			title: this.title,
+			abstract: this.abstract,
+			content: this.content,
+			like: 0,
+			comments: []
+		}
+
+		mongodb.MongoClient.connect(mongodb.url, (err, db) => {
 			if(err) {
 				return callback(err);
 			}
-			callback(null);
-		});
-	});
-};
-
-Post.getTen = (opt, page, callback) => {
-	mongodb.MongoClient.connect(mongodb.url, (err, db) => {
-		if(err) {
-			return callback(err);
-		}
-
-		let collection = db.collection('posts');
-		collection.count(opt, (err, total) => {
-			collection.find(opt, {
-				skip: (page - 1) * 10,
-				limit: 10
-			}).sort({
-				time: -1
-			}).toArray((err, docs) => {
+			let collection = db.collection('posts');
+			collection.insert(post, {safe: true}, err => {
 				db.close();
 				if(err) {
 					return callback(err);
 				}
-				docs.forEach(doc => {
-					doc.content = markdown.toHTML(doc.content);
-				});
-				callback(null, docs, total);
+				callback(null);
 			});
 		});
-	});
-};
+	}
 
-Post.getAllByEmail = (email, page, callback) => Post.getTen({'email': email}, page, callback);
-
-Post.getAllByName = (name, page, callback) => Post.getTen({'name': name}, page, callback);
-
-Post.getAll = (page, callback) => Post.getTen({}, page, callback);
-
-Post.getOne = (title, callback) => {
-	mongodb.MongoClient.connect(mongodb.url, (err, db) => {
-		if(err) {
-			return callback(err);
-		}
-
-		let collection = db.collection('posts');
-		collection.findOne({"title":title}, (err, doc) => {
-			db.close();
+	static getTen(opt, page, callback) {
+		mongodb.MongoClient.connect(mongodb.url, (err, db) => {
 			if(err) {
 				return callback(err);
 			}
-			if(doc){
-				doc.content = markdown.toHTML(doc.content);
-				doc.comments.forEach(comment => {
-					comment.content = markdown.toHTML(comment.content);
+
+			let collection = db.collection('posts');
+			collection.count(opt, (err, total) => {
+				collection.find(opt, {
+					skip: (page - 1) * 10,
+					limit: 10
+				}).sort({
+					time: -1
+				}).toArray((err, docs) => {
+					db.close();
+					if(err) {
+						return callback(err);
+					}
+					docs.forEach(doc => {
+						doc.content = markdown.toHTML(doc.content);
+					});
+					callback(null, docs, total);
 				});
-			};
-			callback(null, doc);
+			});
 		});
-	});
+	}
+
+	static getAllByEmail(email, page, callback) {
+		this.getTen({email: email}, page, callback);
+	}
+
+	static getAllByName(name, page, callback) {
+		this.getTen({name: name}, page, callback);
+	}
+
+	static getAll(page, callback) {
+		this.getTen({}, page, callback);
+	}
+
+	static getOne(title, callback) {
+		mongodb.MongoClient.connect(mongodb.url, (err, db) => {
+			if(err) {
+				return callback(err);
+			}
+
+			let collection = db.collection('posts');
+			collection.findOne({"title":title}, (err, doc) => {
+				db.close();
+				if(err) {
+					return callback(err);
+				}
+				if(doc){
+					doc.content = markdown.toHTML(doc.content);
+					doc.comments.forEach(comment => {
+						comment.content = markdown.toHTML(comment.content);
+					});
+				};
+				callback(null, doc);
+			});
+		});
+	}
+
+	static edit(name, title, callback) {
+		mongodb.MongoClient.connect(mongodb.url, (err, db) => {
+			if(err) {
+				return callback(err);
+			}
+
+			let collection = db.collection('posts');
+			collection.findOne({
+				"name": name,
+				"title": title
+			}, (err, doc) => {
+				db.close();
+				if(err) {
+					return callback(err);
+				}
+				callback(null, doc);
+			});
+		});
+	}
+
+	static update(name, title, content, callback) {
+		mongodb.MongoClient.connect(mongodb.url, (err, db) => {
+			if(err) {
+				return callback(err);
+			}
+			let collection = db.collection('posts');
+			collection.update({
+				"name": name,
+				"title": title
+			}, {$set:{content: content}}, err => {
+				db.close();
+				if(err) {
+					return callback(err);
+				}
+				callback(null);
+			});
+		});
+	}
+
+	static remove(name, title, callback) {
+		mongodb.MongoClient.connect(mongodb.url, (err, db) => {
+			if(err) {
+				return callback(err);
+			}
+
+			let collection = db.collection('posts');
+			collection.remove({
+				"name": name,
+				"title": title
+			}, {w: 1}, (err) => {
+				db.close();
+				if(err) {
+					return callback(err);
+				}
+				callback(null);
+			});
+		});
+	}
 }
 
-Post.edit = (name, title, callback) => {
-	mongodb.MongoClient.connect(mongodb.url, (err, db) => {
-		if(err) {
-			return callback(err);
-		}
 
-		let collection = db.collection('posts');
-		collection.findOne({
-			"name": name,
-			"title": title
-		}, (err, doc) => {
-			db.close();
-			if(err) {
-				return callback(err);
-			}
-			callback(null, doc);
-		});
-	});
-};
+module.exports = Post;
 
-Post.update = (name, title, content, callback) => {
-	mongodb.MongoClient.connect(mongodb.url, (err, db) => {
-		if(err) {
-			return callback(err);
-		}
-		let collection = db.collection('posts');
-		collection.update({
-			"name": name,
-			"title": title
-		}, {$set:{content: content}}, err => {
-			db.close();
-			if(err) {
-				return callback(err);
-			}
-			callback(null);
-		});
-	});
-};
-
-Post.remove = (name, title, callback) => {
-	mongodb.MongoClient.connect(mongodb.url, (err, db) => {
-		if(err) {
-			return callback(err);
-		}
-
-		let collection = db.collection('posts');
-		collection.remove({
-			"name": name,
-			"title": title
-		}, {w: 1}, (err) => {
-			db.close();
-			if(err) {
-				return callback(err);
-			}
-			callback(null);
-		});
-	});
-};
--- models/users.js
+++ models/users.js
@@ -1,53 +1,61 @@
 'use strict'
 
 const mongodb = require('./db');
-function User(user) {
-	this.name = user.name;
-	this.password = user.password;
-	this.email = user.email;
-};
+
+class User {
+	constructor(name, password, email) {
+		this.name = user.name;
+		this.password = user.password;
+		this.email = user.email;
+	}
+
+	save(callback) {
+		let user = {
+			name: this.name,
+			password: this.password,
+			email: this.email
+		};
+		mongodb.MongoClient.connect(mongodb.url, (err, db) => {
+			if(err) {
+				return callback(err);
+			}
+
+			let collection = db.collection('users');
+			collection.insert(user, {safe: true}, (err, user) => {
+				db.close();
+				if(err) {
+					return callback(err);
+				}
+				callback(null, user[0]);
+			});
+		});
+	}
+
+	static get(opt, callback) {
+		mongodb.MongoClient.connect(mongodb.url, (err, db) => {
+			if(err) {
+				return callback(err);
+			}
+
+			let collection = db.collection('users');
+			collection.findOne(opt, (err, user) => {
+				db.close();
+				if(err) {
+					return callback(err);
+				}
+				callback(null, user);
+			});
+		});
+	}
+
+	static getByEmail(email, callback) {
+		this.get({email: email}, callback);
+	}
+
+	static getByName(name, callback) {
+		this.get({name: name}, callback);
+	}
+}
 
 module.exports = User;
 
-User.prototype.save = callback => {
-	let user = {
-		name: this.name,
-		password: this.password,
-		email: this.email
-	};
-	mongodb.MongoClient.connect(mongodb.url, (err, db) => {
-		if(err) {
-			return callback(err);
-		}
-
-		let collection = db.collection('users');
-		collection.insert(user, {safe: true}, (err, user) => {
-			db.close();
-			if(err) {
-				return callback(err);
-			}
-			callback(null, user[0]);
-		});
-	});
-};
-
-User.get = (opt, callback) => {
-	mongodb.MongoClient.connect(mongodb.url, (err, db) => {
-		if(err) {
-			return callback(err);
-		}
-
-		let collection = db.collection('users');
-		collection.findOne(opt, (err, user) => {
-			db.close();
-			if(err) {
-				return callback(err);
-			}
-			callback(null, user);
-		});
-	});
-};
-
-User.getByEmail = (email, callback) => User.get({'email': email}, callback);
-
-User.getByName = (name, callback) => User.get({'name': name}, callback);
